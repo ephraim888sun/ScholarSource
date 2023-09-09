2dbbc8dea36d933281ad7377d5f947746cbde33e
ephraim888sun <ephraim888sun@gmail.com>
2023-09-09 18:55:00 -0400

    create grant

--- client/thirdweb-app/context/index.js
+++ client/thirdweb-app/context/index.js
@@ -18,7 +18,7 @@ export const StateContextProvider = ({ children }) => {
   const address = useAddress()
   const connect = useMetamask()
 
-  const publishCampaign = async (form) => {
+  const publishGrant = async (form) => {
     try {
       const data = await createGrant({
         args: [
@@ -37,57 +37,55 @@ export const StateContextProvider = ({ children }) => {
     }
   }
 
-  const getCampaigns = async () => {
-    const campaigns = await contract.call('getCampaigns')
+  const getGrants = async () => {
+    const grants = await contract.call('getGrants')
 
-    const parsedCampaings = campaigns.map((campaign, i) => ({
-      owner: campaign.owner,
-      title: campaign.title,
-      description: campaign.description,
-      target: ethers.utils.formatEther(campaign.target.toString()),
-      deadline: campaign.deadline.toNumber(),
+    const parsedGrants = grants.map((grant, i) => ({
+      owner: grant.owner,
+      title: grant.title,
+      description: grant.description,
+      target: ethers.utils.formatEther(grant.target.toString()),
+      deadline: grant.deadline.toNumber(),
       amountCollected: ethers.utils.formatEther(
-        campaign.amountCollected.toString()
+        grant.amountCollected.toString()
       ),
-      image: campaign.image,
+      image: grant.image,
       pId: i,
     }))
 
-    return parsedCampaings
+    return parsedGrants
   }
 
-  const getUserCampaigns = async () => {
-    const allCampaigns = await getCampaigns()
+  const getUserGrants = async () => {
+    const allGrants = await getGrants()
 
-    const filteredCampaigns = allCampaigns.filter(
-      (campaign) => campaign.owner === address
-    )
+    const filteredGrants = allGrants.filter((grant) => grant.owner === address)
 
-    return filteredCampaigns
+    return filteredGrants
   }
 
   const donate = async (pId, amount) => {
-    const data = await contract.call('donateToCampaign', [pId], {
+    const data = await contract.call('sponsorGrant', [pId], {
       value: ethers.utils.parseEther(amount),
     })
 
     return data
   }
 
-  const getDonations = async (pId) => {
-    const donations = await contract.call('getDonators', [pId])
+  const getSponsors = async (pId) => {
+    const donations = await contract.call('getSponsors', [pId])
     const numberOfDonations = donations[0].length
 
-    const parsedDonations = []
+    const parsedSponsors = []
 
     for (let i = 0; i < numberOfDonations; i++) {
-      parsedDonations.push({
+      parsedSponsors.push({
         donator: donations[0][i],
         donation: ethers.utils.formatEther(donations[1][i].toString()),
       })
     }
 
-    return parsedDonations
+    return parsedSponsors
   }
 
   return (
@@ -96,11 +94,11 @@ export const StateContextProvider = ({ children }) => {
         address,
         contract,
         connect,
-        createGrant: publishCampaign,
-        getCampaigns,
-        getUserCampaigns,
+        createGrant: publishGrant,
+        getGrants,
+        getUserGrants,
         donate,
-        getDonations,
+        getSponsors,
       }}
     >
       {children}
---
+++ client/thirdweb-app/pages/create-grant/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateGrant from '../../components/createGrant'
+
+const Index = () => {
+  return (
+    <div>
+      <CreateGrant />
+    </div>
+  )
+}
+
+export default Index

455ee977877b69dab205a3489fa3fa4c049ae01f
Este-flan13 <eg7075@princeton.edu>
2023-09-09 22:16:19 -0400

    Changed home and display

---
+++ client/thirdweb-app/components/DisplayGrants.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { loader } from '../public';
+import { useRouter } from 'next/navigation';
+
+const DisplayGrants = ({ title, isLoading, grants }) => {
+    const router = useRouter();
+
+    const handleRouter = (grant) => { 
+        route(` /grant-details/${grant.title}`, { state: 
+        grant })
+    }
+
+    return (
+        <div>
+            <h1 className="font-epilogue font semi-bold text-[18px]
+            text-white text-left">{title} ({grants.length})</h1>
+
+            <div className="flex flex-wrap mt-[20px] gap-[26px]">
+            {isLoading && (
+                <img src={loader} alt="loader" className="w-[100px]
+                h-[100px] object-contain"/>
+            )}
+
+            {!isLoading && grants.length === 0 && (
+                <p className="font-epilogue font-semibold text-[14px]
+                leading-[30px] text-[#818183]">
+                    You do not have any grants at the moment.
+                </p>
+            )}
+
+            {!isLoading && grants.length > 0 && grants.map
+            ((grant) => <FundCard
+            key={grant.id}
+            {...grant}
+            handelClick={() => handleRouter(grant)}
+            />)}
+            </div>
+        </div>
+    )
+}
+
+export default DisplayGrants
--- client/thirdweb-app/pages/index.js
+++ client/thirdweb-app/pages/index.js
@@ -2,17 +2,25 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 import Link from 'next/link'
 import Sidebar from '../components/sidebar'
 import Navbar from '../components/navbar'
+import  Home from '../components/home'
 
-export default function Home() {
+export default function index() {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10 relative'>
         <Sidebar />
       </div>
 
+      
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
         <Navbar />
+
+
+        <Home />
       </div>
+      
+
+      
     </div>
   )
 }

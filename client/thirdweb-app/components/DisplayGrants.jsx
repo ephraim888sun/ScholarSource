f9ef3387a7fa75e2c767e7856e6c8180a5b2d9a7
ephraim888sun <ephraim888sun@gmail.com>
2023-09-09 23:04:45 -0400

    refactor grant

--- client/thirdweb-app/components/DisplayGrants.jsx
+++ client/thirdweb-app/components/DisplayGrants.jsx
@@ -3,41 +3,55 @@ import React from 'react'
 import FundCard from './FundCard';
 import { loader } from '../public';
 import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid'
 
 const DisplayGrants = ({ title, isLoading, grants }) => {
-    const router = useRouter();
+  const router = useRouter()
 
-    const handleRouter = (grant) => { 
-        route(`/grant-details/${grant.title}`, { state: grant })
-    }
+  const handleRouter = (grant) => {
+    route(`/grant-details/${grant.title}`, { state: grant })
+  }
 
-    return (
-        <div>
-            <h1 className="font-epilogue font semi-bold text-[18px]
-            text-white text-left">{title} ({grants.length})</h1>
+  return (
+    <div>
+      <h1
+        className='font-epilogue font semi-bold text-[18px]
+            text-white text-left'
+      >
+        {title} ({grants.length})
+      </h1>
 
-            <div className="flex flex-wrap mt-[20px] gap-[26px]">
-            {isLoading && (
-                <img src={loader} alt="loader" className="w-[100px]
-                h-[100px] object-contain"/>
-            )}
+      <div className='flex flex-wrap mt-[20px] gap-[26px]'>
+        {isLoading && (
+          <img
+            src={loader}
+            alt='loader'
+            className='w-[100px]
+                h-[100px] object-contain'
+          />
+        )}
 
-            {!isLoading && grants.length === 0 && (
-                <p className="font-epilogue font-semibold text-[14px]
-                leading-[30px] text-[#818183]">
-                    You do not have any grants at the moment.
-                </p>
-            )}
+        {!isLoading && grants.length === 0 && (
+          <p
+            className='font-epilogue font-semibold text-[14px]
+                leading-[30px] text-[#818183]'
+          >
+            You do not have any grants at the moment.
+          </p>
+        )}
 
-            {!isLoading && grants.length > 0 && grants.map
-            ((grant) => <FundCard
-            key={grant.id}
-            {...grant}
-            handelClick={() => handleRouter(grant)}
-            />)}
-            </div>
-        </div>
-    )
+        {!isLoading &&
+          grants.length > 0 &&
+          grants.map((grant) => (
+            <FundCard
+              key={uuidv4()}
+              {...grant}
+              handelClick={() => handleRouter(grant)}
+            />
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default DisplayGrants
